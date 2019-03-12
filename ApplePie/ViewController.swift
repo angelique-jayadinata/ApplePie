8789151700277fdaacced6c6e9187beb43da780c
Angelique <angeliquejayadinata@nhstu.piagetacademy.org>
2019-03-12 15:24:23 +0700

    UI with connections

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
+var listOfWords = ["dog", "cat", "turtle", "fish"]
+let incorrectMovesAllowed = 7
+var totalWins = 0
+var totalLosses = 0
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
